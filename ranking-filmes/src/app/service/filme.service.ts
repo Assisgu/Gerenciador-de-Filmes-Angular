50376dae23cf7be7daa05077a7f3a7a7bfe981ba
Guilherme <guilima102013@gmail.com>
2022-06-14 23:52:16 -0300

    Fazendo backend e chamadas REST

--- ranking-filmes/src/app/service/filme.service.ts
+++ ranking-filmes/src/app/service/filme.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Filme } from '../model/filme';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilmeService {
+  //Injetando HtppClient
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  listar(): Observable<Filme[]>{
+    return this.http.get<Filme[]>(`${environment.apiEndpoint}/filme`);
+  }
+
+  inserir(filme: Filme): Observable<Filme> {
+     return this.http.post<Filme>(`${environment.apiEndpoint}/filme`, filme);
+  }
+
+  remover(id: number): Observable<any> {
+    return this.http.delete(`${environment.apiEndpoint}/filme/${id}`);
+  }
+
+  atualizar(filme: Filme): Observable<Filme> {
+    return this.http.put<Filme>(`${environment.apiEndpoint}/filme/${filme.id}`, filme);  }
 }
--- ranking-filmes/src/app/views/filme/filme.component.ts
+++ ranking-filmes/src/app/views/filme/filme.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Filme } from 'src/app/model/filme';
+import { FilmeService } from 'src/app/service/filme.service';
 
 @Component({
   selector: 'app-filme',
@@ -7,9 +9,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilmeComponent implements OnInit {
 
-  constructor() { }
+  filmes = new Array<Filme>()
+  filme?: Filme;
+  editando = false;
+  colunas = ['nome', 'ano', 'genero', 'nota', 'status', 'acoes'];
+
+  constructor(private FilmeService: FilmeService) { }
+
 
   ngOnInit(): void {
+    this.listar();
+  }
+
+  listar() {
+    //retorna um observable
+    this.FilmeService.listar().subscribe(filmes => {
+      this.filmes = filmes;
+    });
+  }
+
+  novo() {
+    this.filme = new Filme();
+    this.editando = false;
+  }
+
+  salvar() {
+    if (this.filme) {
+      if(!this.editando){
+        this.FilmeService.inserir(this.filme).subscribe(filme => {
+          this.listar();
+          this.filme = undefined;
+        });
+      }
+      else{
+        this.FilmeService.atualizar(this.filme).subscribe(filme => {
+          this.listar();
+          this.filme = undefined; //para sumir form
+        })
+      }
+    }
+  }
+
+  excluir(id: number) {
+    this.FilmeService.remover(id).subscribe(() => {
+      this.listar();
+    });
+  }
+
+  editar(filme: Filme) {
+    this.filme = filme;
+    this.editando = true;
   }
 
 }
